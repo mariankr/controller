69fa64286fb0311c34cce2c5cdb01b3a9adb611f
Marian Krawiecki <marian.krawiecki@gmail.com>
2020-08-18 23:55:13 +0200

    Implementation of simple ObjectBuffer

---
+++ Src/board/utils/objectBuffer.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <cstdint>
+#include <new> // placement new
+
+namespace board::utils
+{
+  template<class T>
+  class ObjectBuffer
+  {
+  public:
+    static constexpr auto SIZE = sizeof(T);
+
+    template<typename... Args>
+    T* construct(Args&&... args)
+    {
+      return new (buf) T(args...);
+    }
+
+    void destruct()
+    {
+      reinterpret_cast<T*>(buf)->~T();
+    }
+
+  private:
+    alignas(8) uint8_t buf[SIZE];
+  };
+
+  namespace usage
+  {
+    class X
+    {
+    public:
+      X() : x(1), ptr("Constructor X()")
+      {
+      }
+      X(int i) : x(i), ptr("Constructor X(int)")
+      {
+      }
+      X(int i, int j) : x(i + j), ptr("Constructor X(int, int)")
+      {
+      }
+
+    private:
+      unsigned x;
+      const char* ptr;
+    };
+    void usage()
+    {
+      using BuferForX = ObjectBuffer<X>;
+      BuferForX xbuf, ybuf, zbuf;
+      X* x = xbuf.construct(); // runs constructor X()
+      X* y = ybuf.construct(1); // runs constructor X(int)
+      X* z = zbuf.construct(1, 2); // runs constructor X(int, int)
+
+      x->~X();
+      y->~X();
+      z->~X();
+    }
+  }
+
+}
